e0cba44f38bbf1a60aacbb042b1ab1b65ddc45d5
Brian Harney <bharney0@gmail.com>
2019-05-24 07:32:31 -0700

    Added nav context.

--- src/components/Common/Nav.js
+++ src/components/Common/Nav.js
@@ -1,53 +1,80 @@
 import React, { lazy, Component, Suspense } from 'react';
 import { Route, Switch, NavLink } from 'react-router-dom'
 import ReactDOM from "react-dom"
+import "../../styles/Nav.scss"
+import { NavContext } from "../../context/NavProvider";
 class Nav extends Component {
-  onClick = () => {
-    let navbar = document.getElementsByClassName("offcanvas-collapse")
-    ReactDOM.findDOMNode(navbar[0]).classList.toggle('open');
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
   }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll() {
+    let offCanvas = document.getElementsByClassName("offcanvas-collapse")[0].classList.contains('open')
+    if (!offCanvas) {
+      let navbar = ReactDOM.findDOMNode(
+        document.getElementById("custom-nav")
+      )
+      let windowsScrollTop = window.pageYOffset;
+      if (windowsScrollTop > 50) {
+        navbar.classList.add("affix");
+        navbar.classList.remove("top-nav-collapse");
+      } else {
+        navbar.classList.remove("affix");
+        navbar.classList.remove("top-nav-collapse");
+      }
+    }
+  }
+
 
   render() {
     const { isAuthenticated, logout } = this.props.auth
-    return (
-      <nav className="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
-        <div className="container">
-          <NavLink to="/" className="navbar-brand" activeClassName="active">React-GraphQL-Starter</NavLink>
-          <button className="navbar-toggler p-0 border-0" type="button" onClick={() => this.onClick()} data-toggle="offcanvas">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-            <ul className="navbar-nav ml-auto">
-              {!isAuthenticated() &&
-                <li key="login" className="nav-item">
-                  <NavLink to="/login" className="nav-link" activeClassName="active" onClick={() => this.onClick()}>Login</NavLink>
-                </li>
-              }
-              {!isAuthenticated() &&
-                <li key="register" className="nav-item">
-                  <NavLink to="/signup" className="nav-link" activeClassName="active" onClick={() => this.onClick()}>Register</NavLink>
-                </li>
-              }
-              {isAuthenticated() &&
-                <li key="add" className="nav-item">
-                  <NavLink to="/add" className="nav-link" activeClassName="active" onClick={() => this.onClick()}>Add</NavLink>
-                </li>}
-              {isAuthenticated() &&
-                <li key="delete" className="nav-item">
-                  <NavLink to="/delete" className="nav-link" activeClassName="active" onClick={() => this.onClick()}>Delete</NavLink>
-                </li>}
-              {isAuthenticated() &&
-                <li key="update" className="nav-item">
-                  <NavLink to="/update" className="nav-link" activeClassName="active" onClick={() => this.onClick()}>Update</NavLink>
-                </li>}
-              {isAuthenticated() &&
-                <li key="logout" className="nav-item">
-                  <NavLink to="/login" className="nav-link" activeClassName="active" onClick={() => { this.onClick(); logout(); }}>Logout</NavLink>
-                </li>}
-            </ul>
-          </div>
-        </div>
-      </nav>
+    return (<NavContext.Consumer>
+      {({ onUpdate, toggle, handleOverlayToggle }) => {
+        return (
+          <nav id="custom-nav" className="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+            <div className="container">
+              <NavLink to="/" className="navbar-brand" onClick={onUpdate} activeClassName="active">React-GraphQL-Starter</NavLink>
+              <button className="navbar-toggler p-0 border-0" type="button" onClick={toggle} data-toggle="offcanvas">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div onClick={(e) => handleOverlayToggle(e)} className="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+                <ul className="navbar-nav ml-auto">
+                  {!isAuthenticated() &&
+                    <li key="login" className="nav-item">
+                      <NavLink to="/login" className="nav-link" activeClassName="active" onClick={onUpdate}>Login</NavLink>
+                    </li>
+                  }
+                  {!isAuthenticated() &&
+                    <li key="register" className="nav-item">
+                      <NavLink to="/signup" className="nav-link" activeClassName="active" onClick={onUpdate}>Register</NavLink>
+                    </li>
+                  }
+                  {isAuthenticated() &&
+                    <li key="add" className="nav-item">
+                      <NavLink to="/add" className="nav-link" activeClassName="active" onClick={onUpdate}>Add</NavLink>
+                    </li>}
+                  {isAuthenticated() &&
+                    <li key="delete" className="nav-item">
+                      <NavLink to="/delete" className="nav-link" activeClassName="active" onClick={onUpdate}>Delete</NavLink>
+                    </li>}
+                  {isAuthenticated() &&
+                    <li key="update" className="nav-item">
+                      <NavLink to="/update" className="nav-link" activeClassName="active" onClick={onUpdate}>Update</NavLink>
+                    </li>}
+                  {isAuthenticated() &&
+                    <li key="logout" className="nav-item">
+                      <NavLink to="/login" className="nav-link" activeClassName="active" onClick={() => { onUpdate(); logout(); }}>Logout</NavLink>
+                    </li>}
+                </ul>
+              </div>
+            </div>
+          </nav>
+        )
+      }}
+    </NavContext.Consumer>
     );
   }
 }
